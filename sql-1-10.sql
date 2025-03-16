840beb9eccc88275d8a7d659ce5c5c416160fb7a
nagadeveloper123 <nagatammavarapudataengineer123@gmail.com>
2025-03-17 04:13:02 +0530

    Add files via upload

---
+++ sql-1-10.sql
@@ -0,0 +1,22 @@
+use zero_analyst;
+
+# 11 Identify returning customers based on their order history. Categorize customers as Returning if they have placed more than one return, 
+    --  and as New otherwise. SELECT * FROM orders11;SELECT * FROM returns11;
+
+# 12 Write a solution to show the unique ID of each user,  If a user does not have a unique ID replace just show null.Return employee name and their unique_id.
+
+# 13 write a SQL query to retrieve all employees  details along with their managers names based on the manager ID SELECT * FROM employees13;
+
+# 14 Find the top 2 customers who have spent the most money across all their orders. SELECT * FROM customers;SELECT * FROM orders14;
+
+# 15 Write an SQL query to retrieve the product details for items whose revenue decreased compared to the previous month. SELECT * FROM orders15;
+
+# 16 Write a SQL query to find the names of managers who have at least five direct reports.Return the result table in any order. SELECT * FROM employees16;
+
+# 17 Write an SQL query to find customers who have made purchases in all product categories.  SELECT * FROM customers17;SELECT * FROM purchases17;
+
+# 18 Write a SQL query to find out each hotal best performing months based on revenue   SELECT * FROM hotel_bookings18;
+
+# 19 Find the details of employees whose salary is greater than the average salary across the entire company.  SELECT * FROM employees19;
+
+# 20 Write a query to find products that are sold by both Supplier A and Supplier B, excluding products sold by only one supplier. select * from products20
